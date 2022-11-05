89bfa48e6e1092354992822853a5c1cad3f954cb
arif <ardwino88@gmail.com>
2022-11-05 10:35:14 +0700

    add: membuat single file

---
+++ view_room.php
@@ -0,0 +1,27 @@
+<?php session_start();
+$title = "SIPERU - Details";
+
+require_once "./partials/head.html";
+?>
+
+<body>
+    <?php
+    require_once "./partials/navbar.html";
+    ?>
+
+    <div class="container">
+        <div class="row mt-4 bg-primary">
+            <div class="col">
+                <h1>haliii</h1>
+            </div>
+            <div class="col">
+                <h1>haloo</h1>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    require_once "./partials/footer.html";
+    ?>
+
+</body>
